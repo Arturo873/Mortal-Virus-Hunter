7ae600569a35dd60340be17a9fc8b7d1d5b0c358
Aasbes <66884023+Aasbes@users.noreply.github.com>
2020-07-28 14:31:25 -0400

    añadio jugador
     el jugador se mueve con W,A,S,D y dispara con E;

---
+++ MortalVirusHunter/Assets/jugador/ControlJugador.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using UnityEngine;
+//control del personaje
+public class ControlJugador : MonoBehaviour
+{   
+    //movimiento personaje
+    public Transform pos;
+    public float speed = 0.5f;
+    public Rigidbody2D rbd;
+    public float salto = 2f;
+    public bool puedeSaltar = false;
+
+    //sprites animacion
+    public SpriteRenderer sprit;
+    //public SpriteRenderer sprit2;
+    public Animator anim;
+
+    //disparo
+    public Transform DisparoDerecha;
+    public Transform DisparoIzquierda;
+    public GameObject balaPrefab;
+
+
+
+    
+    void Start()
+    {
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        
+        //avance a la derecha
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            pos.position += new Vector3(1f, 0, 0) * speed * Time.deltaTime;
+            sprit.flipX = false;
+            anim.SetBool("velocidad", true);
+            //sprit2.flipY = false;
+
+            
+
+        }
+        else
+        {   //avance a la izquierda
+            if (Input.GetKey(KeyCode.A))
+            {
+                
+                pos.position += new Vector3(-1f, 0, 0) * speed * Time.deltaTime;
+                sprit.flipX = true;
+                anim.SetBool("velocidad", true);
+                //sprit2.flipY= true;
+                
+                
+            }
+
+            else
+            {
+                anim.SetBool("velocidad", false);
+
+            }
+        }
+        //salto
+        if (Input.GetKeyDown(KeyCode.W) && puedeSaltar == true)
+        {
+            rbd.AddForce(Vector2.up * salto, ForceMode2D.Impulse);
+            puedeSaltar = false;
+            anim.SetBool("saltar", true);
+
+            
+        }
+        //verifica la direccion para disparar
+        ladoDisparo(sprit);
+
+
+
+    }
+
+    
+    //verifica si toca el suelo
+    public void OnCollisionEnter2D(Collision2D tocando)
+    {
+        if (tocando.gameObject.tag == "suelo")
+        {
+            puedeSaltar = true;
+            anim.SetBool("saltar", false);
+        }
+       
+
+
+
+
+    }
+    
+
+    /*
+    
+    public void OnCollisionExit2D(Collision2D tocando)
+     {
+        if (tocando.gameObject.tag == "suelo")
+     {
+        puedeSaltar = false;
+        anim.SetBool("saltar", true);
+      }
+    }
+
+    */
+
+
+    //disparo
+    public void PlayerShooting(Transform origen)
+    {
+        //if (Input.GetButtonDown("Fire1"))
+        if    (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(balaPrefab, origen.position,origen.rotation);
+            
+        }
+
+
+
+    }
+    //verifica la direccion para disparar
+    public void ladoDisparo(SpriteRenderer sprit)
+    {
+        if(sprit.flipX == false)
+        {
+            PlayerShooting(DisparoDerecha);
+        }
+        else
+        {
+            PlayerShooting(DisparoIzquierda);
+        }
+    }
+
+}
+
+
+
+
+
---
+++ MortalVirusHunter/Assets/jugador/disparo/BalaMovimiento.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class BalaMovimiento: MonoBehaviour
+{
+    public GameObject player;
+
+
+    private Rigidbody2D rbd;
+    public float velocidad;
+    public float duracion;//duracion objeto en pantalla
+
+
+    
+
+
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        rbd = GetComponent<Rigidbody2D>();
+
+        player = GameObject.FindGameObjectWithTag("jugador");
+
+
+
+
+
+
+
+
+    }
+
+    void Start()
+    {
+
+        if (player.GetComponent<SpriteRenderer>().flipX == true)
+        {
+            //disparo izquierda
+            rbd.velocity = new Vector2(-velocidad, rbd.velocity.y);
+        }
+        else
+        {
+            //disparo derecha
+            rbd.velocity = new Vector2(velocidad, rbd.velocity.y);
+        }
+        
+       
+       
+          
+       
+       
+          
+        
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Destroy(gameObject, duracion);
+    }
+
+    //sistema de particulas al colisionar
+    void OnTriggerEnter2D(Collider2D tocando)
+    {
+        if (tocando.tag != "jugador")
+        {
+            //se activan particulas al colisionar
+            GetComponent<ParticleSystem>().Play();
+
+            //se desactivan para evitar otras coliciones
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+
+        }
+            
+    }
+}
