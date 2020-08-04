c98833516b78174540c2df8d2ddaf0aeda73aa93
Aasbes <66884023+Aasbes@users.noreply.github.com>
2020-08-04 19:05:10 -0400

    menus
    un menu de inicio y otro para seleccionar el nivel

---
+++ MortalVirusHunter/Assets/Scenes/menus/New Folder/menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public GameObject flecha, lista;
+    int indice = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dibujar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool up = Input.GetKeyDown("w");
+        bool down = Input.GetKeyDown("s");
+
+        if (up) indice--;
+        if (down) indice++;
+
+        if (indice > lista.transform.childCount - 1) indice = 0;
+        else if (indice < 0) indice = lista.transform.childCount - 1;
+        if (up || down) Dibujar();
+    }
+
+    void Dibujar()
+    {
+
+        Transform opcion = lista.transform.GetChild(indice);
+        flecha.transform.position = opcion.position;
+    }
+}
---
+++ MortalVirusHunter/Assets/Scenes/menus/controlador.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlador : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+
+    }
+}
